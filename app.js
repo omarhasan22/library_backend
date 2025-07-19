de495457644e5b292240249a16b62e67915a7cbd
OmarMHasan <Omarhasan_22@outlook.com>
2025-07-19 23:33:45 +0300

    feat: Log production database URI for improved visibility

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.json());
 const isProduction = process.env.NODE_ENV === 'production';
 const dbURI = isProduction ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_LOCAL;
 console.log(`Using database URI: ${dbURI}`);
+console.log(process.env.MONGO_URI_PROD);
 
 if (!dbURI) {
 	console.error('❌ Database URI is missing!');
